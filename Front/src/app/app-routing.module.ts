c62521d639c8d1139eafd616ff5312cdffcb587f
IRamNoc <90531307+IRamNoc@users.noreply.github.com>
2024-04-04 23:42:32 +0200

    Init projet Angular

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { SearchMoviesComponent } from './components/search-movies/search-movies.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { ManageGroupsComponent } from './components/manage-groups/manage-groups.component';
+import { UserPreferencesComponent } from './components/user-preferences/user-preferences.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: AuthenticationComponent },
+  { path: 'search', component: SearchMoviesComponent},
+  { path: 'movie/:id', component: MovieDetailsComponent},
+  { path: 'groups', component: ManageGroupsComponent },
+  { path: 'preferences', component: UserPreferencesComponent},
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { SearchMoviesComponent } from './components/search-movies/search-movies.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { ManageGroupsComponent } from './components/manage-groups/manage-groups.component';
+import { UserPreferencesComponent } from './components/user-preferences/user-preferences.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    SearchMoviesComponent,
+    MovieDetailsComponent,
+    ManageGroupsComponent,
+    UserPreferencesComponent
   ],
   imports: [
     BrowserModule,
