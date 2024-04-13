6d1552fa0ab3ad5daacd1c841eb157fe9997931b
Tarchaud <101600128+Tarchaud@users.noreply.github.com>
2024-04-13 17:42:16 +0200

    Update groups_routes.py

--- API/routes/groups_routes.py
+++ API/routes/groups_routes.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel, Field
 from typing import List, Optional
 from uuid import UUID, uuid4
-import mysql.connector
 
 from controllers.groups_controllers import getAllGroups, createGroup
 
@@ -12,22 +11,6 @@ class Group(BaseModel):
     id: UUID = Field(default_factory=uuid4)
     name: str
     user_ids: List[UUID]
-
-# Connexion à la base de données MySQL
-def connect_to_database():
-    try:
-        db_connection = mysql.connector.connect(
-            host="mysql",
-            user="your_username",
-            password="your_password",
-            database="watchOurMoviesDB"
-        )
-        return db_connection
-    except mysql.connector.Error as err:
-        print(f"MySQL connection error: {err}")
-        return None
-
-db_connection = connect_to_database()
 
 # Endpoint pour récupérer tous les groupes depuis la base de données
 @router.get("/", response_model=List[Group])
