a4105077be3ea04e783bc6842fba80302efe9962
IRamNoc <90531307+IRamNoc@users.noreply.github.com>
2024-04-13 21:13:20 +0200

    fix rebase

--- API/main.py
+++ API/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
-from movie_routes import router as movie_router
-from user_routes import router as user_router
-from user_movies_routes import router as user_movies_router
-from group_routes import router as group_router
+from routes.movies_routes import router as movie_router
+from routes.users_routes import router as user_router
+from routes.user_movies_routes import router as user_movies_router
+from routes.groups_routes import router as group_router
+from identity_provider.auth import auth_router
 
 app = FastAPI()
 
+app.include_router(auth_router) # Ajout des routes d'authentification
 app.include_router(movie_router) # Ajout des routes films
 app.include_router(user_router) # Ajout des routes user
 app.include_router(user_movies_router) # Ajout des routes user_movies
