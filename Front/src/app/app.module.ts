f03a647f15f7c6305fd60d439d04a25fdd1c7e91
IRamNoc <90531307+IRamNoc@users.noreply.github.com>
2024-04-13 19:54:53 +0200

    add popup connection

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -9,12 +9,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
 import { IndexComponent } from './pages/index/index.component';
 import { HeaderComponent } from './pages/header/header.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +25,8 @@ import { HeaderComponent } from './pages/header/header.component';
     HttpClientModule,
     CommonModule,
     BrowserAnimationsModule,
-    MatDialogModule
+    MatDialogModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/src/app/pages/header/header.component.ts
+++ Front/src/app/pages/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +10,23 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   userName: string = 'Dorian';
 
+  constructor(public dialog: MatDialog) {}
+
   onSearchKeyUp(event: any) {
-      console.log(event.target.value);
+    console.log(event.target.value);
   }
 
   onSearch() {
-      console.log('Recherche effectuée');
+    console.log('Recherche effectuée');
+  }
+
+  openLoginModal() {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      width: '340px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 }
