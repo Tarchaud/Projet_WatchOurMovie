38cd9e2440d5a329ce7e586214f980928c92a0ee
Tarchaud <101600128+Tarchaud@users.noreply.github.com>
2024-04-13 17:58:50 +0200

    Update users_routes.py

--- API/routes/users_routes.py
+++ API/routes/users_routes.py
@@ -6,15 +6,6 @@ import mysql.connector
 import time
 
 from controllers.users_controllers import getAllUsers, createUser
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_DATABASE = os.getenv("DB_DATABASE")
 
 router = APIRouter( prefix="/users", tags=["users"])
 
@@ -29,20 +20,7 @@ class UserRes(BaseModel):
     username: str
     email: str
 
-# Connexion à la base de données MySQL
-def connect_to_database():
-    try:
-        db_connection = mysql.connector.connect(
-            host="mysql",
-            user="your_username",
-            password="your_password",
-            database="watchOurMoviesDB"
-        )
-        return db_connection
-    except mysql.connector.Error as err:
-        print(f"MySQL connection error: {err}")
-        return None
-    
+
 
 # Endpoint pour récupérer tous les utilisateurs depuis la base de données
 @router.get("/", response_model=List[UserRes])
