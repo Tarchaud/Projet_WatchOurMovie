b3f44af8a7db161da7a211f336593494e4bd10a2
Dorian <dorian.carlone@yahoo.fr>
2024-04-14 16:45:58 +0200

    chore: added language selector + navbar search

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Upload System';
+
+  constructor() { }
+
+  ngOnInit(): void {
+      if(!localStorage.getItem('language')) {
+        localStorage.setItem('language', 'en');
+      }
+  }
 }
