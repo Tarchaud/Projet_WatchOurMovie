294980f4f09d87918599e00e099e53bced19ef87
Tarchaud <101600128+Tarchaud@users.noreply.github.com>
2024-04-14 10:58:09 +0200

    update route get movies

--- API/routes/movies_routes.py
+++ API/routes/movies_routes.py
@@ -10,13 +10,11 @@ router = APIRouter( prefix="/movies", tags=["movies"])
 
 # Route pour obtenir le détails d'un film
 @router.get("/{movie_id}/details")
-async def get_movie_details(movie_id: int, movie_data: dict):
+async def get_movie_details(movie_id: int, language : Optional[str] = "fr-FR"):
     """
     Obtention des détails d'un film par ID.
     Renvoie le détail du film correspondant à l'ID donné.
     """ 
-
-    language = movie_data.get("language") if movie_data.get("language") else "fr-FR"
 
     params = {
         "language": language,
@@ -33,15 +31,11 @@ async def get_movie_details(movie_id: int, movie_data: dict):
 
 
 @router.get("/search")
-async def search_movie_by_title(data_req: dict):
+async def search_movie_by_title(title: str, language: Optional[str] = "fr-FR", page: Optional[int] = 1):
     """
     Rechercher un film par titre.
     Renvoie une liste de films correspondant au titre donné.
     """
-
-    title = data_req.get("title") if data_req.get("title") else ""
-    language = data_req.get("language") if data_req.get("language") else "fr-FR"
-    page = data_req.get("page") if data_req.get("page") else 1
 
     params = {
         "query": title,
@@ -60,14 +54,11 @@ async def search_movie_by_title(data_req: dict):
 
 
 @router.get("/trending")
-async def get_trending_movies(movie_data: dict):
+async def get_trending_movies(language: Optional[str] = "fr-FR", time_window: Optional[str] = "day"):
     """
     Obtenir une liste de films en tendance.
     Renvoie la liste des films en tendance.
     """
-
-    language = movie_data.get("language") if movie_data.get("language") else "fr-FR"
-    time_window = movie_data.get("time_window") if movie_data.get("time_window") else "days"
 
     if time_window not in ["day", "week"]:
         time_window = "day"
@@ -90,19 +81,15 @@ async def get_trending_movies(movie_data: dict):
 
 
 @router.get("/genre/movie")
-async def get_list_genre_movies(movie_data: dict):
+async def get_list_genre_movies(language: Optional[str] = "fr-FR"):
     """
     Obtenir une liste des genres de film.
     Renvoie la liste des genre de film.
     """
 
-    language = movie_data.get("language") if movie_data.get("language") else "fr-FR"
-    page = movie_data.get("page") if movie_data.get("page") else 1
-
     # Paramètres de la requête
     params = {
-        "language": language,
-        "page": page,
+        "language": language
     }
     
     try:
@@ -116,18 +103,14 @@ async def get_list_genre_movies(movie_data: dict):
 
 
 #INFO Permet de récupérer les films par genre peut être la modif pour l'utiliser avec les autres paramètres qui sont possible pour cette route de l'API TMDB (30 paramètres possibles)
-# with_genres = genre_id1,genre_id2,genre_id3 => permet de récupérer les films qui ont un des genres spécifiés
-# with_genres = genre_id1|genre_id2|genre_id3 => permet de récupérer les films qui ont tous les genres spécifiés
+# with_genres = genre_id1,genre_id2,genre_id3 => permet de récupérer les films qui ont tous les genres spécifiés
+# with_genres = genre_id1|genre_id2|genre_id3 => permet de récupérer les films qui ont un des genres spécifiés
 @router.get("/genre")
-async def get_movies_by_genre(movie_data: dict):
+async def get_movies_by_genre(language: Optional[str] = "fr-FR", with_genres: str = "", page: Optional[int] = 1):
     """
     Obtenir une liste de films selon le genre spécifié.
     Renvoie la liste des films du genre donné.
     """
-
-    language = movie_data.get("language") if movie_data.get("language") else "fr-FR"
-    with_genres = movie_data.get("genre_id")
-    page = movie_data.get("page") if movie_data.get("page") else 1
 
     params = {
         "language": language,
@@ -137,10 +120,12 @@ async def get_movies_by_genre(movie_data: dict):
         "page": page,
         "sort_by" : "popularity.desc"
     }
+    print(params)
     
     try:
         # Récupérer les films par genre depuis le cache
-        cache_key = ("Movie_Genre", with_genres, language)
+        cache_key = ("Movie_Genre", with_genres, language, page)
+        print(cache_key)
         response = fetch_TMDB_movie_genre(params, cache_key)
         return response
     except requests.exceptions.RequestException as e:
