c7bcc185298e952f002eff75faa83caf02149f27
IRamNoc <90531307+IRamNoc@users.noreply.github.com>
2024-04-14 15:15:54 +0200

    fix

--- API/main.py
+++ API/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= ["http://localhost:4200"],
+    allow_origins= ["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
